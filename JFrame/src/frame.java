c1c8833e71f9b8e84dea92c84c74f8c3c33f67f1
Jhon Paul <jplmaceda2021@plm.edu.ph>
2022-10-22 15:08:30 +0800

    added

---
+++ JFrame/src/frame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class frame {
+
+	public static void main(String args []) {
+		
+		JFrame frame = new JFrame(); //Frame instance
+		
+		frame.setSize(420, 420); // Set the size of frame (x , y)
+		frame.setTitle("JFrame"); // Sets the frame title
+		
+		frame.setVisible(true); // 1st set the frame visibility to true.
+		
+		
+	}
+}
